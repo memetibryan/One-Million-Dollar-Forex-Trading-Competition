e6bba30a6211c47063436211a566a44528db34f4
memetibryan <memetibryan@gmail.com>
2017-10-10 10:20:53 +0300

    added schema files for seminar and conference database seeding and also added their models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171009141559) do
+ActiveRecord::Schema.define(version: 20171010071134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,14 @@ ActiveRecord::Schema.define(version: 20171009141559) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "conferences", force: :cascade do |t|
+    t.text "name"
+    t.text "email"
+    t.text "telephone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "east_africa_awards", force: :cascade do |t|
     t.text "company_name"
     t.text "company_website"
@@ -49,6 +57,14 @@ ActiveRecord::Schema.define(version: 20171009141559) do
     t.integer "ticket_quantity"
     t.integer "security_code"
     t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "seminars", force: :cascade do |t|
+    t.text "name"
+    t.text "email"
+    t.text "telephone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
